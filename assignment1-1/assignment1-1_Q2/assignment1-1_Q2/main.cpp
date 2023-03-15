2e63c8fdfcdaa5b42a499ac89038c965aabfe37d
Nagnero <davidyeon123@naver.com>
2023-03-15 09:46:22 +0900

    [구현완료.ver1] 1주차 문제 2번 주석 추가 및 완료

--- assignment1-1/assignment1-1_Q2/assignment1-1_Q2/main.cpp
+++ assignment1-1/assignment1-1_Q2/assignment1-1_Q2/main.cpp
@@ -4,29 +4,57 @@ using namespace std;
 
 int main()
 {
-	float a, b, c;
+	float a, b, c, discriminant, x1, x2;
 
-	cout << "Enter a, b, c: ";
-	cin >> a >> b >> c;
+	// 입력값 받기 (a, b, c)
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << "Enter c: ";
+	cin >> c;
 
-	cout << "the roots of " << a << "x^2 + " << b << "x + " << c << " = 0 :" << endl << endl;
+	// a, b, c로 판별식 계산 후 discriminant에 저장
+	discriminant = pow(b, 2) - 4 * a * c;
 
+	cout << endl << "the roots of " << a << "x^2 + " << b << "x + " << c << " = 0 :" << endl << endl;
+
+	// a가 0이면 문구 출력 후 프로그램 종료
 	if (a == 0)
 	{
 		cout << "Unexpected factor of a quadratic term" << endl;
+		return 0;
 	}
-	else if (pow(b, 2) - 4 * a * c == 0)
+	
+	// 판별식이 0이면 중근을 출력
+	if (discriminant == 0)
 	{
-		cout << "X1, X2: " << -b / 2 * a <<endl;
+		cout << "X1, X2: " << -b / (2 * a) << endl;
 	}
-	else if (pow(b, 2) - 4 * a * c == 0 < 0)
+	// 판별식이 0보다 작으면 근이 없다고 출력
+	else if (discriminant < 0)
 	{
 		cout << "The equation has no real number solution" << endl;
 	}
+	// 판별식이 0 초과일 때 
 	else
 	{
-		float temp = pow(b, 2) - 4 * a * c;
-		cout << "X1 = " << (-b + sqrt(temp)) / 2 * a << ", X2 = " << (-b - sqrt(temp)) / 2 * a << endl;
+		// b가 양수인 경우 근의공식 사용
+		if (b > 0)
+		{
+			// x1을 유리화해서 계산하고, x2를 그대로 계산
+			x1 = (-2 * c) / (b + sqrt(discriminant));
+			x2 = (-b - sqrt(discriminant)) / (2 * a);
+		}
+		// b가 음수인 경우 (b == 0 이면 판별식도 음수이므로 고려 x
+		else
+		{
+			// x1을 그대로 계산하고, x2를 유리화해서 계산
+			x1 = (-b + sqrt(discriminant)) / (2 * a);
+			x2 = (-2 * c) / (b - sqrt(discriminant));
+		}
+		// 결과 출력
+		cout << "X1: " << x1 << ",   X2: " << x2 << endl;
 	}
 
 	return 0;
