8f96704a157dcd4f4d76c7b986607b1814c06831
unknown <davidyeon123@naver.com>
2023-03-23 21:57:13 +0900

    [구현] 2주차 문제 3번 구현

--- assignment1-2/assignment1-2_Q3/assignment1-2_Q3/main.cpp
+++ assignment1-2/assignment1-2_Q3/assignment1-2_Q3/main.cpp
@@ -1,53 +1,58 @@
-#include <fstream>
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
 #include <string.h>
+
 using namespace std;
 
-bool compare_string(char* input1, char* input2);
+bool wildcard(char* input, char* data) {
 
-int main() {
-    char data[100], input[100], result[20][100]{};
-    int cnt;
+	for (int i = 0; input[i]; i++) {
+		if (input[i] == '*')
+			return 1;
 
-    cout << "Input file name : ";
-    cin >> input >> cnt;
+		if (input[i] == data[i] || input[i] == '?')
+			continue;
+		else {
+			return 0;
+		}
+	}
 
-    ifstream fin;
-    if (strcmp("filename_list.txt", input) == 0) {
-        fin.open("filename_list.txt");
-    }
-    else {
-        cout << "no match file";
-    }
-
-    if (fin) {
-        cout << "no contents" << endl;
-        fin.close();
-        return 0;
-    }
-
-    for (int i = 0; i < cnt; i++) {
-        cin >> input;
-        while (!fin.eof()) {
-            
-            fin.getline(data, 100);
-
-            if (compare_string(input, data)) {
-                *result[i] = *data;
-                
-            }
-        }
-    }
-
-    while (!fin.eof()) {
-        fin.getline(data, 100);
-        cout << data << endl;
-    }
-
-    fin.close();
+	return 1;
 }
 
-bool compare_string(char* input1, char* input2) {
-    cout << input1 << input2;
-    return 1;
+int main() {
+	char data[100], input[100], result[20][100]{};
+	int num, cnt = 0;
+	ifstream fin;
+
+	cout << "Input File name : ";
+	cin >> input >> num;
+
+	if (strcmp(input, "filename_list.txt") == 0)
+		fin.open("filename_list.txt");
+	else {
+		cout << "no matching file";
+		fin.close();
+	}
+
+	// num만큼 반복
+	for (int i = 0; i < num; i++) {
+		cin >> input; // input변수 재활용
+
+		while (!fin.eof()) {
+			fin.getline(data, 100);
+			if (wildcard(input, data)) {
+				strcpy(result[cnt], data);
+				cnt++;
+			}
+		}
+		fin.close();
+		fin.open("filename_list.txt");
+	}
+
+	cout << endl;
+	for (int i = 0; i < cnt; i++)
+		cout << result[i] << endl;
+	
 }
