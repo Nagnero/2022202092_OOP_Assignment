303ace2c03c07fe4562a79e9dc15d13826426adc
unknown <davidyeon123@naver.com>
2023-03-24 04:29:06 +0900

    [구현] 2주차 문제 4번 입력값에 따라 파일명 변하는 기능 구현

--- assignment1-2/assignment1-2_Q4/assignment1-2_Q4/main.cpp
+++ assignment1-2/assignment1-2_Q4/assignment1-2_Q4/main.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 
@@ -6,15 +7,15 @@ using namespace std;
 
 uint8_t** memory_alloc2D(size_t height, size_t width);
 int memory_free2D(uint8_t** memory);
-void swap(int& x, int& y);
+void swap(int& x, int& y); // 문자 위치 바꿈 사용자 정의함수
 
 
 int main()
 {
 	const char* IMAGE_NAME = "Barbara_512x512_yuv400_8bit.raw";
 	const char* RESULT_NAME = "result_Barbara_512x512_yuv400_8bit.raw";
-	const char* RESULT_NAME2 = "Barbara_horizontalflip.raw";
-	const char* RESULT_NAME3 = "Barbara_verticalflip.raw";
+	const char* RESULT_NAME2 = "Barbara_horizontalflip.raw"; // 수평반전 파일명
+	const char* RESULT_NAME3 = "Barbara_verticalflip.raw"; // 수직반전 파일명
 	const int IMAGE_HEIGHT = 512, IMAGE_WIDTH = 512;
 	int x1, x2, y1, y2, i, j, crop_width, crop_height;
 
@@ -72,30 +73,57 @@ int main()
 	else if (x1 < x2 && y1 > y2) {
 		swap(y1, y2);
 	}
+
 	// 자른 이미지의 너비와 높이 계산
 	crop_width = x2 - x1;
 	crop_height = y2 - y1;
 
+	// 자른 넓이만큼 output_image에 이차원 배열로 접근하여 데이터 저장
 	for (i = 0; i < crop_height; i++)
 		for (j = 0; j < crop_width; j++)
 			output_image[i][j] = input_image[y1 + i][x1 + j];
-	char RESULT_NAME1[100] = "Barbara_Cropped_.raw";
+
+	// 자른 넓이를 파일명에 나타내기 위한 로직
+	char RESULT_NAME1[30] = "Barbara_Cropped_";
+	// index  계산을 위한 각종 변수 선언 및 초기화
+	int len = strlen(RESULT_NAME1), cnt = 1, temp = crop_width, temp1 = crop_width;
+	while (temp /= 10)
+		cnt++; // 너비 자릿수 세기
+	for (i = 0; i < cnt; i++) { // 너비의 뒷자리부터 배열에 삽입
+		RESULT_NAME1[len + cnt - i - 1] = temp1 % 10 + 48; // char형을 ASKII코드상 숫자로 변환
+		temp1 /= 10;
+	}
+	len = strlen(RESULT_NAME1) + 1;
+	strcat(RESULT_NAME1, "x"); // 파일명 형식상 'x' 추가
+
+	// 높이에 대해 윗 과정 반복
+	cnt = 1; temp = crop_height, temp1 = crop_height;
+	while (temp /= 10)
+		cnt++;
+	for (i = 0; i < cnt; i++) {
+		RESULT_NAME1[len + cnt - i - 1] = temp1 % 10 + 48;
+		temp1 /= 10;
+	}
+	// 마지막에 '.raw' 문자열 추가하여 확장자 추가
+	strcat(RESULT_NAME1, ".raw");
+
+
 	fopen_s(&fp, RESULT_NAME1, "wb");
 	if (fp == 0)
 	{
 		cout << "Failed to open filename(" << RESULT_NAME1 << ")" << endl;
 		return -1;
 	}
+	// 한줄씩 접근하여 자른 너비만큼 데이터 입력
 	for(int i = 0; i < crop_height; i++)
 		fwrite((uint8_t*)output_image[i], sizeof(uint8_t), (size_t)(crop_width), fp);
 	fclose(fp);
 
 
-
 	// horizontally flipped image 
 	for (i = 0; i < IMAGE_HEIGHT; i++)
 		for (j = 0; j < IMAGE_WIDTH; j++)
-			output_image[i][j] = input_image[i][IMAGE_WIDTH - j];
+			output_image[i][j] = input_image[i][IMAGE_WIDTH - j]; // 원본 이미지의 col index를 수정
 	fopen_s(&fp, RESULT_NAME2, "wb");
 	if (fp == 0)
 	{
@@ -109,7 +137,7 @@ int main()
 	// vertically flipped image
 	for (i = 0; i < IMAGE_HEIGHT; i++)
 		for (j = 0; j < IMAGE_WIDTH; j++)
-			output_image[i][j] = input_image[IMAGE_HEIGHT - i - 1][j];
+			output_image[i][j] = input_image[IMAGE_HEIGHT - i - 1][j]; // 원본 이미지의 row index를 수정
 	fopen_s(&fp, RESULT_NAME3, "wb");
 	if (fp == 0)
 	{
@@ -119,11 +147,8 @@ int main()
 	fwrite((uint8_t*)output_image[0], sizeof(uint8_t), (size_t)(IMAGE_WIDTH * IMAGE_HEIGHT), fp);
 	fclose(fp);
 
-
-
 	memory_free2D(output_image);
 	memory_free2D(input_image);
-
 
 	return 0;
 }
