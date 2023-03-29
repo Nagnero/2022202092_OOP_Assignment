7d6f6be351b79a4e90ad79a86f1edffe1498f4c3
Nagnero <davidyeon123@naver.com>
2023-03-29 21:43:57 +0900

    [구현] 3주차 문제 3번 주석추가

--- assignment1-3/assignment1-3_Q3/assignment1-3_Q3/main.cpp
+++ assignment1-3/assignment1-3_Q3/assignment1-3_Q3/main.cpp
@@ -5,24 +5,27 @@
 using namespace std;
 
 // 입력값을 10진수로 변환하는 함수의 prototype
-int to_decimal(int& temp, int len, int c); 
+int to_decimal(char* input, int& temp, int len, int c);
 // 변환한 10진수를 다시 다른 진법으로 변환하는 함수의 prototype
-int to_target(int temp, int c);
+int to_target(int temp, int t);
 int to_hexa(int temp);
 
-char input[100]{};
-
 int main() {
+    char input[100]{};// 입력을 받을 배열
     int c, t, len, temp = 0; // 현재 진수인 c와 목표 진수인 t
+
+    // 사용자에게 입력값 받기
     cout << "Enter input : ";
     cin >> input >> c >> t;
 
-    len = strlen(input); // 입력값의 자릿수
+    len = strlen(input); // 입력값의 자릿수를 len에 저장
 
-    // 먼저 입력값을 10진수로 변환해서 정수값 도출
-    to_decimal(temp, len, c); 
+    // 먼저 입력값을 10진수로 변환하는 함수 호출
+    // temp에 변환된 정수를 저장 (pass by reference)
+    to_decimal(input, temp, len, c); 
 
     // 변환된 10진수를 목표 진법으로 변환 후 출력
+    // 변환할 목표 진수에 따라 호출 함수 다름
     if (t != 16)
         to_target(temp, t); // 2, 8, 10 진수로 출력
     else
@@ -32,10 +35,11 @@ int main() {
 }
 
 // 십진수 변환
-int to_decimal(int& temp, int len, int c) {
+int to_decimal(char* input, int& temp, int len, int c) {
     // 16진수가 아닌 수라면 공식에 따라 10진수로 변환
     if (c != 16) {
         for (int i = 0; i < len; i++) {
+            // 16진수가 아니라면 입력값을 하나씩 접근하여 자릿수에 따라 10진수 변환
             temp += (input[i] - '0') * pow(c, len - 1 - i);
         }
     }
@@ -46,6 +50,7 @@ int to_decimal(int& temp, int len, int c) {
                 temp += num * pow(c, len - 1 - i);
             }
             else {
+                // 알파벳이 아닌 16진수면 숫자 그대로 10진수로 변환
                 temp += (input[i] - '0') * pow(c, len - 1 - i);
             }
         }
@@ -53,7 +58,6 @@ int to_decimal(int& temp, int len, int c) {
 
     return 0;
 }
-
 
 // 십진수를 원하는 수로 변경 후 출력
 int to_target(int temp, int t) {
