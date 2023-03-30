79c2374cb96102249019c794d83a4a6dac80c0c3
Nagnero <davidyeon123@naver.com>
2023-03-30 22:43:40 +0900

    [구현] 3주차 문제 2번 순환소수 출력 기능 구현

--- assignment1-3/assignment1-3_Q2/assignment1-3_Q2/main.cpp
+++ assignment1-3/assignment1-3_Q2/assignment1-3_Q2/main.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
-int pre_cal(int& numerator, int& denominator);
+int cal(int num, int de_num, int cnt);
+bool check_repeat(int cnt); // 출력값이 반복되는지 확인
 int GCD(int a, int b);
+
+int output[50];
 
 int main() {
     double Vout;
-    int R1, R2, Rload = 0, Vs, numerator, denominator, gcd;
+    int R1, R2, Rload = 0, Vs, num, de_num, gcd;
 
     cout << "Vs: ";
     cin >> Vs;
@@ -20,45 +23,103 @@ int main() {
     cin >> Rload;
 
     if (Rload == 0) {
-        numerator = Vs * R2;
-        denominator = R1 + R2;
-        Vout = (double)numerator / denominator;
+        num = Vs * R2;
+        de_num = R1 + R2;
+        Vout = (double)num / de_num;
     }
     else {
-        numerator = Vs * R2 * Rload * (Rload + R2);
-        denominator = (R1 * (Rload + R2) + R2 * Rload) * (Rload + R2);
-        Vout = (double)numerator / denominator;
+        num = Vs * R2 * Rload * (Rload + R2);
+        de_num = (R1 * (Rload + R2) + R2 * Rload) * (Rload + R2);
+        Vout = (double)num / de_num;
     }
 
-    cout << "Vout: " << numerator << "/" << denominator;
+    cout << "Vout: " << num << "/" << de_num;
     
-    if (numerator <= denominator)
-        gcd = GCD(denominator, numerator);
+    if (num <= de_num)
+        gcd = GCD(de_num, num);
     else
-        gcd = GCD(numerator, denominator);
+        gcd = GCD(num, de_num);
 
     // 분모 분자를 기약분수로 나타내기
-    numerator /= gcd;
-    denominator /= gcd;
+    num /= gcd;
+    de_num /= gcd;
+  
 
-    cin >> numerator >> denominator;
+    // 정수부 저장
+    int pass_num, integer = num / de_num, cnt = 0;
+    if (integer != 0)
+        pass_num = (num % de_num) * 10;
+    else
+        pass_num = num * 10;
 
-    pre_cal(numerator, denominator);
 
-    cout << "Vout: " << numerator << "/" << denominator;
+    // 유한소수인지 판단하기 위해
+    // 분모를 약수 중 2와 5의 개수 카운팅
+    int num_2 = 0, num_5 =0, temp = de_num;
+    while (temp) {
+        if (temp % 2 == 0) {
+            num_2++;
+            temp /= 2;
+        }
+        else
+            break;
+    }
+    temp = de_num;
+    while (temp) {
+        if (temp % 5 == 0) {
+            num_5++;
+            temp /= 5;
+        }
+        else
+            break;
+    }
+    
+    temp = de_num;
+    for (int i = 0; i < num_2; i++)
+        temp /= 2;
+    for (int i = 0; i < num_5; i++)
+        temp /= 5;
+    
+    if (temp == 1) // 분모의 소인수가 2와 5밖에 없으면 유한소수
+        cout << "유한소수";
+    else {
+        output[cnt++] = pass_num / de_num; // 0번째에 1 저장
+        pass_num = (pass_num % de_num) * 10; // 30 저장
+        output[cnt++] = pass_num / de_num; // 1번째에 4 저장
+        pass_num = (pass_num % de_num) * 10; // 20 저장
+        cnt = cal(pass_num, de_num, cnt);
+    }
 
+    cout << " = " << integer << ".(";
+    for (int i = 0; i < cnt; i++)
+        cout << output[i];
+    cout << ")";
+    
 
+    return 0;
 }
 
-int pre_cal(int& numerator, int& denominator) {
-    if (numerator < denominator)
-        return 0;
-    else if (numerator / 10 == 0)
+int cal(int num, int de_num, int cnt) {
+    if (check_repeat(cnt))
+        return cnt / 2;
+
+    output[cnt++] = num / de_num;
+    cal((num % de_num) * 10, de_num, cnt);
+}
+
+bool check_repeat(int cnt) {
+    if (cnt % 2 == 1)
         return 0;
 
-    numerator /= 10;
-    pre_cal(numerator, denominator);
+    int mid = cnt / 2;
+    for (int i = 0; i < mid; i++) {
+        if (output[i] != output[mid + i])
+            return 0;
+    }
+
+    return 1;
 }
+
 
 int GCD(int a, int b) {
     if (b == 0)
