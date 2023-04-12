8d8afbad253043005063e1b4f9e8298b027b6cb3
Nagnero <davidyeon123@naver.com>
2023-04-12 19:49:56 +0900

    [구현] 5주차 문제 3번 구현

--- assignment2-2/assignment2-2_Q3/assignment2-2_Q3/CSVLoader.cpp
+++ assignment2-2/assignment2-2_Q3/assignment2-2_Q3/CSVLoader.cpp
@@ -4,41 +4,114 @@
 using namespace std;
 
 CSVLoader::CSVLoader() {
-	this->data = NULL;
+	this->data = nullptr;
 	this->cols = 0;
 	this->rows = 0;
 }
 
 CSVLoader::CSVLoader(const char* path) {
-	FILE* fin = fopen(path, "r");
-
-
-
-	fclose(fin);                                                                                    
+	this->data = nullptr;
+	this->cols = 0;
+	this->rows = 0;
+	this->parse(path);
 }
 
 CSVLoader::~CSVLoader() {
+	for (int i = 0; i < this->getCols(); i++)
+		delete[] this->data[i];
 
+	delete[] this->data;
 }
 
 
 float** CSVLoader::getData() const {
-
+	return this->data;
 }
 
 int CSVLoader::getRows() const {
-
+	return this->rows;
 }
 
 int CSVLoader::getCols() const {
-
+	return this->cols;
 }
 
 
 void CSVLoader::parse(const char* path) {
+	ifstream fin;
+	string input;
+	int max_col = 0, max_row = 0;
+
+	fin.open(path);
+
+	// 몇열 몇행인지 판단
+	while (getline(fin, input)) {
+		int row = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (input[i] != ',') {
+				// temp.push_back(input[i]);
+				continue;
+			}
+			else {
+				// this->data[col][row] = stof(temp);
+				row++;
+				// temp = "";
+			}
+		}
+		max_col++;
+		if (row > max_row)
+			max_row = row;
+	}
+
+	this->cols = max_col;
+	this->rows = max_row + 1;
+
+	fin.close();
+
+	// 행과 열 계산 후 해당 크기만큼 동적배열 할당
+	this->data = new float* [getCols()];
+	for (int i = 0; i < getCols(); i++) {
+		this->data[i] = new float[getRows()];
+		for (int j = 0; j < getRows(); j++)
+			this->data[i][j] = 0;
+	}
+
+	// 값 입력을 위해 다시 파일 오픈
+	fin.open(path);
+
+	string temp = "";
+	int col = 0;
+
+	while (getline(fin, input)) {
+		int row = 0;
+		for (int i = 0; i < input.length() + 1; i++) {
+			if (input[i] != ',' && input[i] != '\0') {
+				temp.push_back(input[i]);
+			}
+			else {
+				if (temp == "") {
+					row++;
+					continue;
+				}
+
+				this->data[col][row] = stof(temp);
+				row++;
+				temp = "";
+			}
+		}
+		col++;
+	}
+
+	fin.close();
 
 }
 
 void CSVLoader::print(void) {
 
+	for (int i = 0; i < getCols(); i++) {
+		for (int j = 0; j < getRows(); j++) {
+			cout << this->data[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
--- assignment2-2/assignment2-2_Q3/assignment2-2_Q3/main.cpp
+++ assignment2-2/assignment2-2_Q3/assignment2-2_Q3/main.cpp
@@ -3,11 +3,34 @@
 
 using namespace std;
 
+
 int main() {
 
-	const char* path = "CSVLoad.txt";
-
+	const char* path = "CSVLoader_Data.txt";
+	string input;
+	int command = 0, col = 0, row = 0;
+	
 	CSVLoader csv(path);
+	float** dataSet = csv.getData();
+	cout << "Total column and row is " << csv.getRows() << " and " << csv.getCols() << endl << endl;
+
+	while (1) {
+		cout << "Enter Command (1.print all, 2.print with index(col,row), 3.exit): ";
+		cin >> command;
+
+		if (command == 1) {
+			csv.print();
+			cout << endl;
+		}
+		else if (command == 2) {
+			cin >> col >> row;
+			cout << dataSet[col - 1][row - 1] << endl;
+		}
+		else if (command == 3)
+			break;
+		else
+			cout << "Enter vaild command. " << endl;
+	}
 
 	return 0;
 }
