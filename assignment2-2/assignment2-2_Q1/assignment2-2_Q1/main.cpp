cbd1b2af6537624a0f4b0808674b9710e7ca93d5
Nagnero <davidyeon123@naver.com>
2023-04-12 21:22:29 +0900

    [수정] 5주차 문제 1번 구조체로 수정

--- assignment2-2/assignment2-2_Q1/assignment2-2_Q1/main.cpp
+++ assignment2-2/assignment2-2_Q1/assignment2-2_Q1/main.cpp
@@ -3,40 +3,52 @@
 
 using namespace std;
 
-class Node {
-private:
-	string str;
-	Node* next;
-	Node* d_next;
+typedef struct Node {
+	string data = "";
+	struct Node* next = nullptr;
+	struct Node* d_next = nullptr;
+	struct Node* prev = nullptr;
+} Node;
 
-public:
-	Node() {
-		str = "";
-		next = NULL;
-		d_next = NULL;
-	}
-
-	void setInput(string input) { this->str = input; }
-	string getStr() { return this->str; }
-	void setNext(Node* nextNode) { this->next = nextNode; }
-	Node* getNext() { return this->next; }
-};
 
 
 int main() {
-	int start = 0, end = 0;
-	string input;
-	Node* arr = new Node;
+	string input, temp;
 
-	getline(cin, input);
+	cin >> input;
 
-	for (int i = 0; i < input.length(); i++) {
-		if (input[i] = '[') { // 배열의 시작을 알리면
-			start++;
+	// 첫번째는 항상 [로 시작되므로 조건문 생략하고 시작 노드 생성
+	Node* head = new Node;
+	Node* curNode = head;
+
+	// 입력받은 문자열을 리스트에 저장하는 반복문
+	for (int i = 1; i < input.length(); i++) {
+		// 새로운 리스트 선언
+		if (input[i] == '[') {
+			Node* newNode = new Node;
+			
+		}
+		// 콤마를 만나면 temp를 배열에 저장
+		else if (input[i] == ',') {
+			i++; // comma뒤에 항상 공백이 오기 때문
 			Node* newNode = new Node;
 
+			if (temp == "") { // 빈 문자열이면 그대로 리스트 추가
+
+			}
+		}
+		// 리스트 나가기
+		else if (input[i] == ']') {
+
+		}
+		// 문자열의 요소면
+		else {
+			// 삽입할 문자열이 빈 것이면
+			
+			temp.push_back(input[i]);
 		}
 	}
 
-	delete arr;
+
+	delete head;
 }
