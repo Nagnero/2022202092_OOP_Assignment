7a920c2bbd259edc10d833331e2d0aa9bd178afc
unknown <davidyeon123@naver.com>
2023-04-13 20:38:32 +0900

    [구현] 5주차 문제 1번 링크드 리스트로 입력받기 및 전체 배열 출력 기능 구현

--- assignment2-2/assignment2-2_Q1/assignment2-2_Q1/main.cpp
+++ assignment2-2/assignment2-2_Q1/assignment2-2_Q1/main.cpp
@@ -3,52 +3,160 @@
 
 using namespace std;
 
-typedef struct Node {
-	string data = "";
-	struct Node* next = nullptr;
-	struct Node* d_next = nullptr;
-	struct Node* prev = nullptr;
-} Node;
+class Node {
+private:
+    string str;
+    Node* next;
+    Node* d_next;
 
+public:
+    Node() {
+        str = "";
+        next = NULL;
+        d_next = NULL;
+    }
+
+    void setInput(string input) { this->str = input; }
+    string getInput() { return this->str; }
+    void setNext(Node* nextNode) { this->next = nextNode; }
+    void d_setNext(Node* d_nextNode) { this->d_next = d_nextNode; }
+    Node* getNext() { return this->next; }
+    Node* d_getNext() { return this->d_next; }
+};
+
+class List {
+private:
+    Node* head;
+    int size;
+    Node* tail;
+public:
+    List() {
+        this->head = NULL;
+        this->tail = NULL;
+        size = 0;
+    }
+    ~List() {
+        Node* curNode = head;
+        Node* delNode = NULL;
+        while (curNode != NULL) {
+            delNode = curNode;
+            curNode = curNode->getNext();
+            delete delNode;
+        }
+    }
+    void INSERT(string data);
+    void d_INSERT(List* temp_list);
+    void Print_all(Node* curNode);
+    Node* getHead() { return this->head; }
+};
+
+void List::INSERT(string data) {
+    Node* newNode = new Node;
+    newNode->setInput(data);
+
+    if (head == NULL) {
+        tail = head = newNode;
+        this->size++;
+    }
+    else {
+        tail->setNext(newNode);
+        tail = newNode;
+        this->size++;
+    }
+}
+
+void List::d_INSERT(List* temp_list) {
+    Node* newNode = new Node;
+    tail->setNext(newNode);
+    tail = tail->getNext();
+
+    tail->d_setNext(temp_list->head);
+}
+
+void List::Print_all(Node* curNode) {
+
+    while (curNode != NULL) {
+        if (curNode->d_getNext()) {
+            cout << "[";
+            Print_all(curNode->d_getNext());
+            cout << "]";
+        }
+
+
+        if (curNode->getNext()) {
+            cout << curNode->getInput() << ", ";
+        }
+        else
+            cout << curNode->getInput();
+
+        curNode = curNode->getNext();
+    }
+}
+
+
+int parse(List* Linked_list, string input, int i, int level) {
+    string temp = "";
+
+    for (; i < input.length();) {
+        if (input[i] == '[') { // 배열 시작을 만나면 
+            i++;
+            List* temp_list = new List;
+            i = parse(temp_list, input, i, level);
+            Linked_list->d_INSERT(temp_list);
+        }
+        else if (input[i] == ']') { // 배열 탈출을 만나면
+            Linked_list->INSERT(temp); // temp에 저장된 값을 삽입
+            temp = "";
+            i = i + 3;
+            return i;
+        }
+        else if (input[i] == ',') {
+            i = i + 2; // 콤마와 공백은 세트이므로
+            Linked_list->INSERT(temp);
+            temp = "";
+
+        }
+        else {
+            temp.push_back(input[i]);
+            i++;
+        }
+    }
+}
 
 
 int main() {
-	string input, temp;
+    List* Linked_list = new List;
+    string input, temp, command = "";
+    int begin = 1, end = 0, level = 1, i = 1;
 
-	cin >> input;
+    getline(cin, input);
 
-	// 첫번째는 항상 [로 시작되므로 조건문 생략하고 시작 노드 생성
-	Node* head = new Node;
-	Node* curNode = head;
+    parse(Linked_list, input, i, level);
 
-	// 입력받은 문자열을 리스트에 저장하는 반복문
-	for (int i = 1; i < input.length(); i++) {
-		// 새로운 리스트 선언
-		if (input[i] == '[') {
-			Node* newNode = new Node;
-			
-		}
-		// 콤마를 만나면 temp를 배열에 저장
-		else if (input[i] == ',') {
-			i++; // comma뒤에 항상 공백이 오기 때문
-			Node* newNode = new Node;
-
-			if (temp == "") { // 빈 문자열이면 그대로 리스트 추가
-
-			}
-		}
-		// 리스트 나가기
-		else if (input[i] == ']') {
-
-		}
-		// 문자열의 요소면
-		else {
-			// 삽입할 문자열이 빈 것이면
-			
-			temp.push_back(input[i]);
-		}
-	}
+    while (command != "exit") {
+        cin >> command;
+        if (command == "arr") { // 전체 배열 출력
+            cout << '[';
+            Linked_list->Print_all(Linked_list->getHead());
+            cout << ']' << endl;
+        }
+        else { // 배열 지정해서 출력 할 시
+            int cnt = 0, index[100] {}; // 100차원 배열까지 출력 가능
+            for (int i = 3; i < command.length(); i++) 
+                if (command[i] == '[') {
+                    temp = "";
+                    i++;
+                    while (command[i] != ']') {
+                        temp.push_back(command[i]);
+                        i++;
+                    }
+                    index[cnt++] = stoi(temp);
+                }
 
 
-	delete head;
+        }
+    }
+
+
+
 }
