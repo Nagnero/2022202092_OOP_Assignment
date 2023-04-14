e2dfedf74ac1b08dee2c26f37af6b8ed976fc52a
Nagnero <davidyeon123@naver.com>
2023-04-14 14:25:50 +0900

    [주석] 5주차 문제 4번 주석 추가

--- assignment2-2/assignment2-2_Q4/assignment2-2_Q4/Clock.cpp
+++ assignment2-2/assignment2-2_Q4/assignment2-2_Q4/Clock.cpp
@@ -3,53 +3,57 @@
 
 using namespace std;
 
+// 생성자
 Clock::Clock() {
 	this->hour = 0;
 	this->minute = 0;
 	this->second = 0;
 }
 
+// 소멸자
 Clock::~Clock() {
 	cout << "clock is destroyed" << endl;
 }
 
-
+// 시간 증가 멤버 함수
 void Clock::increaseHour() { 
-	if (this->hour < 23)
+	if (this->hour < 23) // 시간이 22이하면 1 증가
 		this->hour++;
-	else
+	else // 23이면 0으로 설정
 		this->setHour(0);
 }
+// 분 증가 멤버 함수
 void Clock::increaseMinute() { 
-	if (this->minute < 59)
+	if (this->minute < 59) // 분이 58이하면 1 증가
 		this->minute++;
-	else {
+	else { // 분이 59면 0으로 설정 후 시간 증가
 		this->setMinute(0);
 		increaseHour();
 	}
 }
+// 초 증가 멤버 함수
 void Clock::increaseSecond() {
-	if (this->second < 59)
+	if (this->second < 59) // 초가 58이하면 1 증가
 		this->second++;
-	else {
+	else { // 초가 59면 0으로 설정 후 분 증가
 		this->setSecond(0);
 		increaseMinute();
 	}
 }
 
-
-void Clock::setTime(int second) {
-	this->setSecond(second % 60);
+// 시간 설정 멤버함수
+void Clock::setTime(int second) { // 초를 전달 받아서
+	this->setSecond(second % 60); // 시, 분, 초로 처리 후 각각 설정
  	this->setMinute((second / 60) % 60);
 	this->setHour((second / 3600) % 24);
 }
 
-
+// 시간 출력 멤버 함수
 void Clock::printTime() {
 	printf("%02d:%02d:%02d\n", this->hour, this->minute, this->second);
 }
 
-
+// 시 분 초 설정 멤버 함수
 void Clock::setHour(int hour) { this->hour = hour; }
 void Clock::setMinute(int minute) { this->minute = minute; }
 void Clock::setSecond(int second) { this->second = second; }
--- assignment2-2/assignment2-2_Q4/assignment2-2_Q4/main.cpp
+++ assignment2-2/assignment2-2_Q4/assignment2-2_Q4/main.cpp
@@ -4,37 +4,37 @@
 using namespace std;
 
 int main() {
-	Clock* clock = new Clock;
+	Clock* clock = new Clock; // clock 객체 동적 할당
 	int sec, command = 1;
 
 	cout << "Enter second: ";
-	cin >> sec;
+	cin >> sec; // 초 입력
 
-	clock->setTime(sec);
-	clock->printTime();
+	clock->setTime(sec); // clock 객체에서 초를 입력 받아 시분초 계산해서 저장
+	clock->printTime(); // 시간 출력
 
-	while (1) {
+	while (1) { // 무한반복
 		cout << "Enter Command (1.Increase hour, 2.Increase minute, 3.Increase second, 0. exit): ";
-		cin >> command;
-		if (command == 1) {
+		cin >> command; // command 저장
+		if (command == 1) { // 시간 1 증가 후 출력
 			clock->increaseHour();
 			clock->printTime();
 		}
-		else if (command == 2) {
+		else if (command == 2) { // 분 1 증가 후 출력
 			clock->increaseMinute();
 			clock->printTime();
 		}
-		else if (command == 3) {
+		else if (command == 3) { // 초 1 증가 후 출력
 			clock->increaseSecond();
 			clock->printTime();
 		}
-		else if (command == 0)
+		else if (command == 0) // 반복문 탈출
 			break;
 		else
 			cout << "Enter valid number" << endl;
 	}
 
-	delete clock;
+	delete clock; // 객체 메모리 할당 해제
 
 	return 0;
 }
