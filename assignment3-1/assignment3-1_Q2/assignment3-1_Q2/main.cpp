08a5c1a7b222ef34810fd98415194d1f434d9438
Nagnero <davidyeon123@naver.com>
2023-05-04 13:08:33 +0900

    [주석] 7주차 문제 2번 주석 추가

--- assignment3-1/assignment3-1_Q2/assignment3-1_Q2/main.cpp
+++ assignment3-1/assignment3-1_Q2/assignment3-1_Q2/main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
+// my_strtok 함수 선언
 char* my_strtok(char* str);
 
 int main() {
-	char str[200]{};
-	int num = 0;
+	char str[200]{}; // 입력받을 문자열
+	int num = 0; // '@' 기호의 개수
 
-	cin >> str;
+
+	cin >> str; // 문자열 입력받기
 
 	// @의 개수를 확인하는 반복문
 	for (int i = 0; str[i]; i++)
@@ -25,7 +27,7 @@ int main() {
 	char* token = my_strtok(str);
 
 	// 반복문을 돌며 출력
-	while (token != 0) {
+	while (*token != '\0') {
 		cout << token << endl;
 		token = my_strtok(NULL); // 한번 값을 전달한 이후엔 nullptr 전달
 	}
@@ -37,12 +39,13 @@ char* my_strtok(char* str) {
 	static char* static_str = NULL;
 
 	if (!str) // 전달받은 매개변수가 NULL이면
-		static_str++;
+		static_str++; // static_str 값을 1 증가
 	else // 전달받은 매개변수가 초기 입력값이면 해당 값 저장
 		static_str = str;
 
-	char* return_tok = static_str;
+	char* return_tok = static_str; // return할 token 값 초기화
 
+	// 입력값에서 @이나 .을 찾을 때까지 문자를 하나씩 읽음
 	while (*static_str != '@' && *static_str != '.' && *static_str != '\0')
 		static_str++;
 
