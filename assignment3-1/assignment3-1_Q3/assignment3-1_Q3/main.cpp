37b9fc73ce743c60945fa432295fb36f70302953
Nagnero <davidyeon123@naver.com>
2023-05-04 13:16:54 +0900

    [주석] 7주차 문제 3번 주석 추가

--- assignment3-1/assignment3-1_Q3/assignment3-1_Q3/main.cpp
+++ assignment3-1/assignment3-1_Q3/assignment3-1_Q3/main.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 
 
-#include <Windows.h>
-
-#define _CRTDBG_MAP_ALLOC
-#include <cstdlib>
-#include <crtdbg.h>
-
-#ifdef _DEBUG
-#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-#endif
-
-
 using namespace std;
 
 class Node {
 private:
-	Node* m_pNext;
-	int m_Data;
+	Node* m_pNext; // 다음 노드의 주소를 저장할 포인터
+	int m_Data; // 현재 노드에 저장될 데이터
 
 public:
-	Node() {
+	Node() { // 생성자
 		this->m_pNext = NULL;
 		this->m_Data = 0;
 	}
-	~Node() {}
+	~Node() {} // 소멸자
 
 	void SetData(int n) { this->m_Data = n; }
 	void SetNext(Node* pNext) { this->m_pNext = pNext; }
@@ -34,31 +23,31 @@ public:
 
 class Queue {
 private:
-	Node* m_pHead;
-	Node* m_pTail;
-	int m_Size;
-	int m_NumElemnet;
+	Node* m_pHead; // 큐의 head 노드를 가리킬 포인터
+	Node* m_pTail; // 큐의 tail 노드를 가리킬 포인터
+	int m_Size; // 큐의 크기를 저장할 변수
+	int m_NumElemnet; // 현재 저장된 노드의 개수를 저장할 변수
 
 public:
-	Queue() {
+	Queue() { // 생성자
 		this->m_pHead = NULL;
 		this->m_pTail = NULL;
 		this->m_Size = 0;
 		this->m_NumElemnet = 0;
 	}
 
-	~Queue() {
-		Node* curNode = m_pHead;
-		Node* delNode = NULL;
+	~Queue() { // 소멸자
+		Node* curNode = m_pHead; // 현재 노드를 가리킬 포인터
+		Node* delNode = NULL; // 삭제할 노드를 가리킬 포인터
 
-		while (curNode) {
+		while (curNode) { // 모든 노드를 삭제하는 반복문
 			delNode = curNode;
 			curNode = curNode->GetNext();
-			if (delNode == m_pTail) {
+			if (delNode == m_pTail) { // tail 노드일 경우
 				this->m_pHead = NULL;
 				this->m_pTail = NULL;
 			}
-			delete delNode;
+			delete delNode; // 노드 삭제
 		}
 	}
 
@@ -107,12 +96,13 @@ Node* Queue::Pop() {
 	}
 }
 
+// 큐에 저장된 모든 데이터를 출력하는 멤버함수
 void Queue::PrintQueue() {
-	Node* curNode = m_pHead;
+	Node* curNode = m_pHead; // 현재 노드를 가리키는 포인터
 
-	while (curNode) {
-		cout << curNode->GetData() << " ";
-		curNode = curNode->GetNext();
+	while (curNode) { // 모든 노드에 대해 반복
+		cout << curNode->GetData() << " "; // 노드에 저장된 데이터 출력
+		curNode = curNode->GetNext(); // 다음 노드로 이동
 	}
 }
 
@@ -121,19 +111,19 @@ int main() {
 	char command[20]{};
 	int input = 0, size;
 
-	cin >> size;
-	queue->SetSize(size);
+	cin >> size; // 큐의 크기를 입력 받음
+	queue->SetSize(size); // 큐의 크기를 설정
 
-	while (1) {
+	while (1) { // 사용자로부터 명령어를 입력받아 처리하는 반복문
 		cin >> command;
 
-		if (strcmp(command, "push") == 0) {
+		if (strcmp(command, "push") == 0) { // push 명령어 처리
 			cin >> input;
 
-			if (!queue->IsFull()) { // 꽉 차 있지 않으면
-				Node* newNode = new Node;
+			if (!queue->IsFull()) { // 큐가 꽉 차 있지 않으면
+				Node* newNode = new Node; // 새로운 노드를 생성
 
-				newNode->SetData(input);
+				newNode->SetData(input); // 입력 받은 데이터를 노드에 저장
 				if (queue->Push(newNode))
 					cout << "Push successful" << endl;
 			}
@@ -153,17 +143,17 @@ int main() {
 				cout << "Already Empty" << endl;
 			}
 		}
-		else if (strcmp(command, "print") == 0) {
-			queue->PrintQueue();
-			cout << endl;
+		else if (strcmp(command, "print") == 0) { // print 명령어 처리
+			queue->PrintQueue(); // 큐에 저장된 모든 데이터를 출력
+			cout << endl; // 개행 문자 출력
 		}
-		else if (strcmp(command, "exit") == 0)
+		else if (strcmp(command, "exit") == 0) // exit 명령어 처리
 			break;
-		else
-			cout << "Enter valid command" << endl;
+		else // 잘못된 명령어를 입력했을 경우 실패 메시지 출력
+			cout << "Enter valid command" << endl; 
 	}
 
 	delete queue;
-	_CrtDumpMemoryLeaks();
+	
 	return 0;
 }
