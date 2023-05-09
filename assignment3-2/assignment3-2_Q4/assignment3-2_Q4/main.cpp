766ad1fcf752be8a4a1980ae019e48b3a271daba
Nagnero <davidyeon123@naver.com>
2023-05-09 10:52:56 +0900

    [구현] 8주차 문제 4번 다항식 추가 기능 구현

--- assignment3-2/assignment3-2_Q4/assignment3-2_Q4/main.cpp
+++ assignment3-2/assignment3-2_Q4/assignment3-2_Q4/main.cpp
@@ -9,9 +9,9 @@ private:
     Term* m_pNext;
 
 public:
-    Term() {
-        this->m_Coefficient = 0;
-        this->m_Exponent = 0;
+    Term(int coeff, int exponent) {
+        this->m_Coefficient = coeff;
+        this->m_Exponent = exponent;
         this->m_pNext = NULL;
     }
     ~Term() {}
@@ -43,22 +43,27 @@ public:
 
 // 삽입 멤버함수 정의
 void Polynomial::Insert(Term* pTerm) {
+    // 첫번째 노드가 아닌 경우
+    if (this->m_pHead) {
+        Term* curNode = this->m_pHead;
+        // 마지막 노드로 curNode 이동
+        while (curNode->getNext())
+            curNode = curNode->getNext();
 
+        curNode->setNext(pTerm);
+    }
+    else { // 첫번째 노드인 경우
+        this->m_pHead = pTerm;
+    }
 }
 
+void createPolynomial(char temp[], Polynomial* Poly) {
 
-
-
-int main() {
-    Polynomial polynomial1, polynomial2;
-    char input[256]{}, temp[256]{};
+    char input[256]{};
     int index = 0, i = 0, j = 0;
 
-    cout << "Enter first polynomial" << endl;
-    cin.getline(temp, 256);
-
     // 입력값에서 공백 제거
-    while (true) {   
+    while (1) {
         if (temp[j] == '\0' || j == 255) { // 개행 문자나 버퍼 크기 제한에 도달한 경우
             break; // 입력 종료
         }
@@ -69,7 +74,6 @@ int main() {
             j++;
     }
 
-
     while (1) {
         // 반복문 탈출 조건
         if (input[index] == '\0')
@@ -78,35 +82,78 @@ int main() {
             index++;
             continue;
         }
-        
+
         int coeff = 0, exponent = 0, sign = 1;
         // coeffi의 부호 확인
         if (input[index] == '-') {
             sign = -1;
             index++;
         }
-        // 앞에서부터 coeff 판단
-        while (input[index] >= '0' && input[index] <= '9') {
-            coeff *= 10;
-            coeff += input[index] - '0';
+        else {
             index++;
+        }
+
+        // 앞에서부터 coeff 판단
+        // 1이 생략되어 있는 경우
+        if (input[index] >= 'A' && input[index] <= 'z') {
+            coeff = 1;
+        }
+        else {
+            while (input[index] >= '0' && input[index] <= '9') {
+                coeff *= 10;
+                coeff += input[index] - '0';
+                index++;
+            }
         }
 
         // exponent 확인. 변수로 알파벳을 받으면 넘기기
-        if (input[index] >= 'A' && input[index] <= 'z') 
+        if (input[index] >= 'A' && input[index] <= 'z') {          
             index++;
-        // 지수 ^ 입력 받으면 넘기기
-        if (input[index] == '^')
-            index++;
+            // 지수 ^ 입력 받으면 넘기기
+            if (input[index] == '^')
+                index++;
 
-        // 이후 Exponent 판단
-        while (input[index] >= '0' && input[index] <= '9') {
-            exponent *= 10;
-            exponent += input[index] - '0';
-            index++;
+            // 이후 Exponent 판단
+            while (input[index] >= '0' && input[index] <= '9') {
+                exponent *= 10;
+                exponent += input[index] - '0';
+                index++;
+            }
+
+            if (!exponent)
+                exponent++;
         }
+        
 
+        
+
+        // 첫번째 다항식 저장
+        Term* newTerm = new Term(coeff, exponent);
+        Poly->Insert(newTerm);
     }
+}
+
+
+int main() {
+    Polynomial* polynomial1 = new Polynomial;
+    Polynomial* polynomial2 = new Polynomial;
+    char temp[256]{};
+
+    // 첫번째 다항식 입력
+    cout << "Enter first polynomial" << endl;
+    cin.getline(temp, 256);
+    // 첫번째 다항식 저장
+    createPolynomial(temp, polynomial1);
+
+
+    // 두번째 다항식 입력
+    cout << "Enter second polynomial" << endl;
+    cin.getline(temp, 256);
+    // 두번째 다항식 저장
+    createPolynomial(temp, polynomial2);
+
+    delete polynomial1;
+    delete polynomial2;
 
     return 0;
 }
