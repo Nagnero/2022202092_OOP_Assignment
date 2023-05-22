6fc5d9903d624a0ad24222015a20d438d0a42009
Nagnero <davidyeon123@naver.com>
2023-05-22 13:46:27 +0900

    [구현] 9주차 문제 1번 구현 완료

--- assignment3-3/assignment3-3_Q1/assignment3-3_Q1/main.cpp
+++ assignment3-3/assignment3-3_Q1/assignment3-3_Q1/main.cpp
@@ -5,82 +5,89 @@ using namespace std;
 
 class Sale {
 private:
-	double Price;
+    double Price;
 
 public:
-	Sale() {
-		this->Price = 0;
-	}
-	Sale(double ThePrice) {
-		this->Price = ThePrice;
-	}
+    Sale() {
+        this->Price = 0;
+    }
+    Sale(double ThePrice) {
+        this->Price = ThePrice;
+    }
 
-	double GetPrice() { return this->Price; }
-	virtual double Bill();
-	double Savings(Sale& Other);
-	bool operator<(Sale& Other) {
-		return this->GetPrice() < Other.GetPrice();
-	}
+    double GetPrice() { return this->Price; }
+    virtual double Bill() { return GetPrice(); } // 가격 그대로 반환하는 Bill() 함수
+    // 다른 Sale 객체와의 가격 차이를 계산하여 반환하는 Savings() 함수
+    double Savings(Sale& Other) { return Other.GetPrice() - GetPrice(); }
+    // Sale 객체끼리 가격을 비교하는 연산자 오버로딩
+    bool operator<(Sale& Other) { return GetPrice() < Other.GetPrice(); }
 };
 
 class DiscountSale : public Sale {
 private:
-	double Discount;
+    double Discount;
 
 public:
-	DiscountSale() {
-		this->Discount = 0;
-	}
-	DiscountSale(double ThePrice, double theDiscount) {
-		this->Discount = ThePrice * (1 - theDiscount);
-	}
+    DiscountSale() {
+        this->Discount = 0;
+    }
+    DiscountSale(double ThePrice, double theDiscount) : Sale(ThePrice) {
+        this->Discount = theDiscount;
+    }
 
-	double GetDiscount();
-	void SetDiscount();
-	double Bill();
-	double Savings(DiscountSale& Other);
-
+    double GetDiscount() { return Discount; }
+    void SetDiscount(double theDiscount) { Discount = theDiscount; }
+    // 할인된 가격을 계산하여 반환하는 Bill() 함수
+    double Bill() { return GetPrice() * (1 - Discount); }
+    // 다른 Sale 객체와의 할인된 가격 차이를 계산하여 반환하는 Savings() 함수
+    double Savings(Sale& Other) { return Other.GetPrice() - Bill(); }
 };
 
 // 입력받은 퍼센트를 소수로 바꾸는 함수
 double convertToNumber(const string& str) {
-	string numStr;
-	for (char ch : str) {
-		if (ch != '%') {
-			numStr += ch;
-		}
-	}
+    string numStr;
+    for (char ch : str) {
+        if (ch != '%') {
+            numStr += ch;
+        }
+    }
 
-	double number = stod(numStr);
-	return number / 100.0; // 변환된 숫자 값을 백분율로 표현하기 위해 100으로 나누기
+    double number = stod(numStr);
+    return number / 100.0; // 변환된 숫자 값을 백분율로 표현하기 위해 100으로 나누기
 }
 
 int main() {
-	int item1, item2;
-	double percent;
+    double item1, item2;
+    double percent;
 
-	cout << "=======================================" << endl;
-	cout << "Price Compare Program" << endl;
-	cout << "=======================================" << endl;
-	cout << "Insert item1 price: $";
-	cin >> item1;
-	cout << "Insert item2 price: $";
-	cin >> item2;
+    cout << "=======================================" << endl;
+    cout << "Price Compare Program" << endl;
+    cout << "=======================================" << endl;
+    cout << "Insert item1 price: $";
+    cin >> item1;
+    cout << "Insert item2 price: $";
+    cin >> item2;
 
-	Sale* sale1 = new Sale(item1);
-	Sale* sale2 = new Sale(item2);
+    Sale* sale1 = new Sale(item1);
+    Sale* sale2 = new Sale(item2);
 
-	cout << endl << "---------------------------------------" << endl;
-	cout << "Insert discount percent: ";
-	string input;
-	cin >> input;
-	percent = convertToNumber(input);
-	cout << endl << "---------------------------------------" << endl;
+    cout << endl << "---------------------------------------" << endl;
+    cout << "Insert discount percent: ";
+    string input;
+    cin >> input;
+    percent = convertToNumber(input);
+    cout << endl << "---------------------------------------" << endl;
 
-	DiscountSale* discount = new DiscountSale(item1, percent);
+    DiscountSale* discount = new DiscountSale(item1, percent);
 
-	delete sale1;
-	delete sale2;
+    cout << "Item1 price: $" << sale1->GetPrice() << endl;
+    cout << "Item2 price: $" << sale2->GetPrice() << endl;
+    cout << "Item1 bill after discount: $" << discount->Bill() << endl;
+    cout << "Item1 savings compared to Item2: $" << discount->Savings(*sale2) << endl;
 
-	return 0;
+    delete sale1;
+    delete sale2;
+    delete discount;
+
+    return 0;
 }
